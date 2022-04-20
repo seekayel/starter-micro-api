2a5a46225d419f75fabb36db7ed5f0463468b5a2
Kam Lasater <ckl@seekayel.com>
2022-04-19 21:45:25 -0400

    Added display html

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-var http = require('http');
+const http = require('http');
+const axios = require('axios')
+
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}`)
-    console.log(Object.keys(process.env).sort().forEach(k => {console.log(`${k}: ${process.env[k]}`)}))
-    res.write(process.env.MONGO_URL||'Yo!');
+    // console.log(Object.keys(process.env).sort().forEach(k => {console.log(`${k}: ${process.env[k]}`)}))
+    if (process.env.MONGO_URL) {
+        res.write('<html><body><h1>🚫 MONGO_URL not set 🚫</h1></body></html>');
+    } else {
+        res.write('<html><body><h1>🎉 MONGO_URL set! 🎉</h1></body></html>');
+    }
     res.end();
 }).listen(3000);
