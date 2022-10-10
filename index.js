48fb7a0d87de7b33dc7951663f0a20a00a63b690
Kam Lasater <ckl@seekayel.com>
2022-10-10 15:46:33 -0400

    trying with encoded URI

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express()
 
+
+app.get('/gallery/album/:album', (req, res) => {
+  const album = req.params.album
+  const encodedName = encodeURI(`where={"album":"${album}"}`)
+
+  console.log('album: ',album)
+  console.log('encodedName: ',encodedName)
+  res.status(200).send('Ok')
+})
+
 app.use('*', (req, res) => {
     console.log(`Request for ${req.url}`)
     // console.log(Object.keys(process.env).sort().forEach(k => {console.log(`${k}: ${process.env[k]}`)}))
