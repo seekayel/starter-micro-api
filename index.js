411a8749c38a119c7b7f465eb31b6afc0037d27c
Kam <ckl@seekayel.com>
2023-03-09 15:06:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use('*', (req, res) => {
   console.log(`Request for ${req.url}`)
   // console.log(Object.keys(process.env).sort().forEach(k => {console.log(`${k}: ${process.env[k]}`)}))
   if (req.accepts('json')) {
-    res.json({url: req.originalUrl, path: req.baseUrl+req.path, query: req.query});
+    res.json({auth: false, url: req.originalUrl, path: req.baseUrl+req.path, query: req.query});
   } else {
     res.send('<html><body><h1>Wanting to send json. Sorry.</h1></body></html>');
   }
